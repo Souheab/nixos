c3d24b0046003683918037656cb7a8534b723d6f
Souheab <souheab@protonmail.com>
2025-11-01 21:31:49 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Desktop nixos config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system; config.allowUnfree = true;};
+  in
+  {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix  # Your existing NixOS config file
+      ];
+      specialArgs = { inherit pkgs; };
+    };
+  };
+}
