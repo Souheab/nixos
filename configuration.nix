785db8e4d0842a5d975cecf8e1d4fe58e0bab8a8
Souheab <souheab@protonmail.com>
2025-12-29 06:56:32 -0500

    add community cache

--- configuration.nix
+++ configuration.nix
@@ -106,6 +106,16 @@
 
   security.polkit.enable = true;
 
+  nix.settings.substituters = [
+    "https://cache.nixos.org/"
+    "https://nix-community.cachix.org"
+  ];
+
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
